3a73a21096549861bf74a0dedd0b92eacdb6d1a5
Philip Chang <sgnoohc@gmail.com>
2019-04-04 22:46:22 -0700

    update

--- modules/TriggerModule.cc
+++ modules/TriggerModule.cc
@@ -20,57 +20,79 @@ void wvzModule::TriggerModule::AddOutput()
 
 void wvzModule::TriggerModule::FillOutput()
 {
-    // Triggers
-    tx->createBranch<int>("HLT_DoubleMu");
-    tx->createBranch<int>("HLT_DoubleEl");
-    tx->createBranch<int>("HLT_MuEG");
 
-        if (gconf.year == 2016)
-        {
-            tx->setBranch<int>("HLT_DoubleMu"                  , coreTrigger.HLT_DoubleMu                  );
-            tx->setBranch<int>("HLT_DoubleEl_DZ_2"             , coreTrigger.HLT_DoubleEl_DZ_2             );
-            tx->setBranch<int>("HLT_MuEG"                      , coreTrigger.HLT_MuEG                      );
-        }
+    if (gconf.year == 2016)
+    {
+        tx->setBranch<int>("HLT_DoubleMu" , coreTrigger.HLT_DoubleMu                                );
+        tx->setBranch<int>("HLT_DoubleEl" , coreTrigger.HLT_DoubleEl || coreTrigger.HLT_DoubleEl_DZ );
+        tx->setBranch<int>("HLT_MuEG"     , coreTrigger.HLT_MuEG                                    );
+    }
 
-        else if (gconf.year == 2017)
-        {
-            tx->setBranch<int>("HLT_DoubleMu"                  , coreTrigger.HLT_DoubleMu_2017             );
-            tx->setBranch<int>("HLT_DoubleEl"                  , coreTrigger.HLT_DoubleEl_2017             );
-            tx->setBranch<int>("HLT_DoubleEl_DZ"               , coreTrigger.HLT_DoubleEl_DZ               );
-            tx->setBranch<int>("HLT_DoubleEl_DZ_2"             , coreTrigger.HLT_DoubleEl_DZ_2             );
-            tx->setBranch<int>("HLT_MuEG"                      , coreTrigger.HLT_MuEG_2017                 );
-            tx->setBranch<int>("HLT_MuEG_2016"                 , coreTrigger.HLT_MuEG                      );
-            tx->setBranch<int>("HLT_SingleEl8"                 , coreTrigger.HLT_SingleEl8_2017            );
-            tx->setBranch<int>("HLT_SingleEl17"                , coreTrigger.HLT_SingleEl17_2017           );
-            tx->setBranch<int>("HLT_SingleIsoEl8"              , coreTrigger.HLT_SingleIsoEl8_2017         );
-            tx->setBranch<int>("HLT_SingleIsoEl17"             , coreTrigger.HLT_SingleIsoEl17             );
-            tx->setBranch<int>("HLT_SingleIsoEl23"             , coreTrigger.HLT_SingleIsoEl23_2017        );
-            tx->setBranch<int>("HLT_SingleIsoMu8"              , coreTrigger.HLT_SingleIsoMu8_2017         );
-            tx->setBranch<int>("HLT_SingleIsoMu17"             , coreTrigger.HLT_SingleIsoMu17_2017        );
-            tx->setBranch<int>("HLT_PFMET140_PFMHT140_IDTight" , coreTrigger.HLT_PFMET140_PFMHT140_IDTight );
-        }
+    else if (gconf.year == 2017)
+    {
+        tx->setBranch<int>("HLT_DoubleMu" , coreTrigger.HLT_DoubleMu_2017 );
+        tx->setBranch<int>("HLT_DoubleEl" , coreTrigger.HLT_DoubleEl_2017 );
+        tx->setBranch<int>("HLT_MuEG"     , coreTrigger.HLT_MuEG_2017     );
+    }
 
-        else if (gconf.year == 2018)
-        {
-            tx->setBranch<int>("HLT_DoubleMu"                  , coreTrigger.HLT_DoubleMu_2018             );
-            tx->setBranch<int>("HLT_DoubleEl"                  , coreTrigger.HLT_DoubleEl_2018             );
-            tx->setBranch<int>("HLT_DoubleEl_DZ"               , coreTrigger.HLT_DoubleEl_DZ               );
-            tx->setBranch<int>("HLT_DoubleEl_DZ_2"             , coreTrigger.HLT_DoubleEl_DZ_2             );
-            tx->setBranch<int>("HLT_MuEG"                      , coreTrigger.HLT_MuEG_2018                 );
-            tx->setBranch<int>("HLT_MuEG_2016"                 , coreTrigger.HLT_MuEG                      );
-            tx->setBranch<int>("HLT_SingleEl8"                 , coreTrigger.HLT_SingleEl8_2018            );
-            tx->setBranch<int>("HLT_SingleEl17"                , coreTrigger.HLT_SingleEl17_2018           );
-            tx->setBranch<int>("HLT_SingleIsoEl8"              , coreTrigger.HLT_SingleIsoEl8_2018         );
-            tx->setBranch<int>("HLT_SingleIsoEl17"             , coreTrigger.HLT_SingleIsoEl17             );
-            tx->setBranch<int>("HLT_SingleIsoEl23"             , coreTrigger.HLT_SingleIsoEl23_2018        );
-            tx->setBranch<int>("HLT_SingleIsoMu8"              , coreTrigger.HLT_SingleIsoMu8_2018         );
-            tx->setBranch<int>("HLT_SingleIsoMu17"             , coreTrigger.HLT_SingleIsoMu17_2018        );
-            tx->setBranch<int>("HLT_PFMET140_PFMHT140_IDTight" , coreTrigger.HLT_PFMET140_PFMHT140_IDTight );
-        }
+    else if (gconf.year == 2018)
+    {
+        tx->setBranch<int>("HLT_DoubleMu" , coreTrigger.HLT_DoubleMu_2018 );
+        tx->setBranch<int>("HLT_DoubleEl" , coreTrigger.HLT_DoubleEl_2018 );
+        tx->setBranch<int>("HLT_MuEG"     , coreTrigger.HLT_MuEG_2018     );
+    }
 
+    bool trig_ee = true;
+    bool trig_em = true;
+    bool trig_mm = true;
 
-    int pass_duplicate_se_sm = 0;
-    int pass_duplicate_sm_se = 0;
-    tx->setBranch<int>("pass_duplicate_se_sm", pass_duplicate_se_sm);
-    tx->setBranch<int>("pass_duplicate_sm_se", pass_duplicate_sm_se);
+    if (gconf.year == 2016)
+    {
+        trig_ee = coreTrigger.HLT_DoubleEl || coreTrigger.HLT_DoubleEl_DZ;
+        trig_em = coreTrigger.HLT_MuEG;
+        trig_mm = coreTrigger.HLT_DoubleMu;
+    }
+    else if (gconf.year == 2017)
+    {
+        trig_ee = coreTrigger.HLT_DoubleEl_2017;
+        trig_em = coreTrigger.HLT_MuEG_2017;
+        trig_mm = coreTrigger.HLT_DoubleMu_2017;
+    }
+    else if (gconf.year == 2018)
+    {
+        trig_ee = coreTrigger.HLT_DoubleEl_2018;
+        trig_em = coreTrigger.HLT_MuEG_2018;
+        trig_mm = coreTrigger.HLT_DoubleMu_2018;
+    }
+
+    bool is_pd_ee = (babymaker->looper.getCurrentFileName().Contains("DoubleEG") || babymaker->looper.getCurrentFileName().Contains("EGamma"));
+    bool is_pd_em = babymaker->looper.getCurrentFileName().Contains("MuonEG");
+    bool is_pd_mm = babymaker->looper.getCurrentFileName().Contains("DoubleMuon");
+    bool pass_duplicate_ee_em_mm = false;
+    bool pass_duplicate_mm_em_ee = false;
+
+    if (is_pd_ee)
+    {
+        if (trig_ee)
+            pass_duplicate_ee_em_mm = true;
+        if (!trig_mm && !trig_em && trig_ee)
+            pass_duplicate_mm_em_ee = true;
+    }
+    else if (is_pd_em)
+    {
+        if (!trig_ee && trig_em)
+            pass_duplicate_ee_em_mm = true;
+        if (!trig_mm && trig_em)
+            pass_duplicate_mm_em_ee = true;
+    }
+    else if (is_pd_mm)
+    {
+        if (!trig_ee && !trig_em && trig_mm)
+            pass_duplicate_ee_em_mm = true;
+        if (trig_mm)
+            pass_duplicate_mm_em_ee = true;
+    }
+    tx->setBranch<int>("pass_duplicate_ee_em_mm", pass_duplicate_ee_em_mm);
+    tx->setBranch<int>("pass_duplicate_mm_em_ee", pass_duplicate_mm_em_ee);
+
 }
